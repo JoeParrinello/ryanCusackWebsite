d6f67829e3550b7e159b0368fea0c3c905fbfa7b
Joe Parrinello <joe.parrinello@gmail.com>
2015-03-10 01:18:15 -0400

    added main.css, added new font

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
 
 <html>
     <head>
+        <link href='//fonts.googleapis.com/css?family=PT+Sans:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
         <title>Ryan Cusack</title>
+        <link href='/css/main.css' rel="stylesheet" type="text/css">
     </head>
     <body>
         <h1>Ryan Cusack Personal Website</h1>
