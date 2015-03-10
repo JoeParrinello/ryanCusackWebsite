c12e263de8a6363790d0d38cf79d4f7d02a36eac
Joe Parrinello <joe.parrinello@gmail.com>
2015-03-10 01:25:25 -0400

    Added Temp Favicon

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <head>
         <link href='//fonts.googleapis.com/css?family=PT+Sans:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
         <title>Ryan Cusack</title>
+        <link rel="icon" type="image/x-icon" href="resources/favicon.ico">
         <link href='/css/main.css' rel="stylesheet" type="text/css">
     </head>
     <body>
