891c04c4500a692eb0a3a40575566d04eeddb9ab
Joe Parrinello <joe.parrinello@gmail.com>
2015-03-10 01:09:14 -0400

    Switching to html for the main site

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
-<?php
+
 /**
  * Created by PhpStorm.
  * User: taevis662
  * Date: 3/10/15
  * Time: 1:04 AM
  */
-
-echo "Hello World!";
+<html>
+    <head>
+        <title>Ryan Cusack</title>
+    </head>
+    <body>
+        <h1>Ryan Cusack Personal Website</h1>
+        <h2>In Progress</h2>
+    </body>
+</html>
